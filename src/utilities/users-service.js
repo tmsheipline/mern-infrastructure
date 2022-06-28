ed6cb4364ef8d0b2ab56c25383a004c689653073
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-06-28 09:46:39 -0400

    JSON token

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -6,6 +6,6 @@ export async function signUp(userData){
   // Delegate the network request code to the users-api.js API module
   // which will ultimately return a JSON Web Token (JWT)
   const token = await usersAPI.signUp(userData);
-  // Baby step by returning whatever is sent back by the server
-  return token;  
+  // Persist the "token"
+  localStorage.setItem('token', token);
 }
